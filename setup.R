926c4353f512426fb37c32112abe9c9885a892f3
Matthew Sudmann-Day <mdaygoldenco@hotmail.com>
2015-12-20 19:32:57 +0100

    add some package dependencies

--- setup.R
+++ setup.R
@@ -2,6 +2,8 @@
 # Install R packages used by the application at runtime.
 # This can require more memory than the smallest AWS instance provides.
 
+install.packages("DBI", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
+install.packages("sp", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
 install.packages("ggplot2", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
 install.packages("fossil", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
 install.packages("RMySQL", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
